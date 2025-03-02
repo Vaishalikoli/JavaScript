2cb8950ee5f81a58b2acaafe9963420c5d5634e0
VAISHALI KOLI <vaishalikoli2004@gmail.com>
2025-03-02 20:56:29 +0530

    Add files via upload

---
+++ Logical.js
@@ -0,0 +1,17 @@
+/*  && -> and
+    || -> or
+    ! -> not (reverse)
+*/
+
+let isLoggedin = true;
+let ispaid = false;
+
+console.log(isLoggedin && ispaid); //false
+// console.log(isLoggedin || ispaid); //true
+// console.log(!isLoggedin); //false
+// console.log(!ispaid); //true
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser); //false
