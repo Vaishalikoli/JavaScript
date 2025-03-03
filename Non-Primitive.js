6f78fae4d6f5d2497ef62beef7f47d8674d98bf8
VAISHALI KOLI <vaishalikoli2004@gmail.com>
2025-03-03 20:46:51 +0530

    Add files via upload

---
+++ Non-Primitive.js
@@ -0,0 +1,23 @@
+const username = {
+  "first name": "vaishali",
+  lastname: "Koli",
+  isLoggedin: true,
+};
+
+username.firstname = "Ms.Vaishali";
+username.lastname = "Koli";
+
+console.log(username["first name"]);
+console.log(username.lastname);
+console.log(typeof username);
+console.log(username.firstname);
+console.log(username.lastname);
+
+let todate = new Date();
+console.log(todate);
+
+let anotherUser = ["vaishali", true];
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
